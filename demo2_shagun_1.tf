7120834a834a9f816b14cac2bc96469bfad1b2ef
shagunavid <shagun@avidsecure.io>
2018-12-11 17:07:00 +0530

    Shagun

--- demo2_shagun_1.tf
+++ demo2_shagun_1.tf
@@ -213,6 +213,20 @@ resource "aws_cloudtrail" "example" {
  }
 }
 
+resource "aws_cloudtrail" "example2" {
+ is_multi_region_trail = true
+ enable_logging = true
+ #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ event_selector {
+   read_write_type = "All"
+   include_management_events = true
+   data_resource {
+     type   = "AWS::Lambda::Function"
+     values = ["arn:aws:lambda"]
+   }
+ }
+}
+
 
 #Ensure a log metric filter and alarm exist for unauthorized API calls
 resource "aws_cloudwatch_log_metric_filter" "UnauthorizedAccess" {
