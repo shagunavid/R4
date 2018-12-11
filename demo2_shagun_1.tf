628b04f6ed57e8d9b76a0f477c737f6fe5953eff
shagunavid <shagun@avidsecure.io>
2018-12-11 17:08:31 +0530

    Shagun

--- demo2_shagun_1.tf
+++ demo2_shagun_1.tf
@@ -202,7 +202,7 @@ resource "aws_iam_account_password_policy" "strict_2" {
 resource "aws_cloudtrail" "example" {
  is_multi_region_trail = true
  enable_logging = true
- cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
  event_selector {
    read_write_type = "All"
    include_management_events = true
@@ -227,6 +227,19 @@ resource "aws_cloudtrail" "example2" {
  }
 }
 
+resource "aws_cloudtrail" "example3" {
+ is_multi_region_trail = true
+ enable_logging = true
+ #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ event_selector {
+   read_write_type = "All"
+   include_management_events = true
+   data_resource {
+     type   = "AWS::Lambda::Function"
+     values = ["arn:aws:lambda"]
+   }
+ }
+}
 
 #Ensure a log metric filter and alarm exist for unauthorized API calls
 resource "aws_cloudwatch_log_metric_filter" "UnauthorizedAccess" {
