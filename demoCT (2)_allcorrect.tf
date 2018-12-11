8d5fa25d6bd2df0cea52aa22be1036e885660a05
shagunavid <shagun@avidsecure.io>
2018-12-11 16:24:25 +0530

    Shagun

--- demoCT (2)_allcorrect.tf
+++ demoCT (2)_allcorrect.tf
@@ -202,6 +202,22 @@ resource "aws_iam_account_password_policy" "strict_2" {
 
 
 
+resource "aws_cloudtrail" "example" {
+
+  is_multi_region_trail = true
+
+  #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+  event_selector {
+    read_write_type = "All"
+    include_management_events = true
+
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
+}
+
 
 
 #Ensure a log metric filter and alarm exist for Management Console sign-in without MFA
