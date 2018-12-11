93311f556c1bddb0015e4cce6205a0d0bc21ef4f
shagunavid <shagun@avidsecure.io>
2018-12-11 18:19:08 +0530

    Shagun

--- demo2_shagun_1.tf
+++ demo2_shagun_1.tf
@@ -202,7 +202,7 @@ resource "aws_iam_account_password_policy" "strict_2" {
 resource "aws_cloudtrail" "example" {
  is_multi_region_trail = true
  enable_logging = true
- #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
  event_selector {
    read_write_type = "All"
    include_management_events = true
@@ -216,7 +216,7 @@ resource "aws_cloudtrail" "example" {
 resource "aws_cloudtrail" "example2" {
  is_multi_region_trail = true
  enable_logging = true
- #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
  event_selector {
    read_write_type = "All"
    include_management_events = true
@@ -230,7 +230,7 @@ resource "aws_cloudtrail" "example2" {
 resource "aws_cloudtrail" "example3" {
  is_multi_region_trail = true
  enable_logging = true
- #cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
+ cloud_watch_logs_group_arn    = "aws:arn::log-group:someLogGroup:"
  event_selector {
    read_write_type = "All"
    include_management_events = true
@@ -242,15 +242,16 @@ resource "aws_cloudtrail" "example3" {
 }
 
 #Ensure a log metric filter and alarm exist for unauthorized API calls
-resource "aws_cloudwatch_log_metric_filter" "UnauthorizedAccess" {
-name           = "console-without-mfa4"
-pattern        = "{ ($.userIdentity.type = \"Root\") && ($.userIdentity.invokedBy NOT EXISTS) && ($.eventType != \"AwsServiceEvent\") }"
-log_group_name = "someLogGroup"
-metric_transformation {
-  name      = "ConsoleWithoutMFACount"
-  namespace = "someNamespace"
-  value     = "1"
-}
+resource "aws_cloudwatch_log_metric_filter" "UnAuthorized" {
+ name           = "console-without-mfa"
+ pattern        = "{$.errorCode = \"*UnauthorizedOperation\" || $.errorCode = \"AccessDenied*\"}"
+ log_group_name = "someLogGroup"
+
+ metric_transformation {
+   name      = "ConsoleWithoutMFACount"
+   namespace = "someNamespace"
+   value     = "1"
+ }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "MFAUsed" {
